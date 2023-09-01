ab749433ccac2c8212c6dafb1893cbf783e00447
Nathan Elliott <nelliott13@users.noreply.github.com>
2023-08-31 21:05:09 -0700

    Expand logging, respect interrupts, update vars

--- scripts/02_analyze_bids.R
+++ scripts/02_analyze_bids.R
@@ -6,59 +6,62 @@ source(file.path(setup_dir, "01_setup.R"))
 
 # Load packages (for multi-core processing and reporting)
 library(future)
+library(foreach)
 library(doFuture)
 library(progressr)
 
 # Split shapefile
 floodarea_files <- split_flooding_area(axn_file_clean,
-                                       field_column_name = "FieldID",  #created in 01_setup.R
+                                       field_column_name = "BidFieldID",  #created in 01_setup.R
                                        guide_raster = ref_file,
-                                       output_dir = spl_dir,           #defined in definitions.R
+                                       output_dir = spl_dir,              #defined in definitions.R
                                        do_rasterize = FALSE,
                                        overwrite = overwrite_global)
 
-floodarea_files <- list.files(spl_dir, pattern = ".shp$", full.names = TRUE)
-fa_rst_files <- list.files(spl_dir, pattern = ".tif$", full.names = TRUE)
-
-# Water files
-
-# Can subset files using the scenarios parameter, which is applied as a regex filter
-scenarios_filter <- "imposed"
-
-cores_to_use <- 6
-n_sessions <- min(length(flood_areas), cores_to_use, availableCores() - 1)
-
-handlers(global = TRUE)
-handlers(handler_progress(
-  format = "[:bar] :percent (:current/:total) - Elapsed: :elapsed, ETA: :eta",
-  clear = FALSE))
-
+# Function that runs the auction, using progress handlers and multiple cores as 
+# set by 'plan' in the future package
 evaluate_auction <- function(flood_areas, overwrite = FALSE, 
                              retry_times = 0, retry_counter = NULL,
                              verbose_level = 1, p = NULL, global_prog_mult = 1) {
   
   # Check/set retry counter
-  if (retry_times > 2) stop(add_ts("Only supports two levels of retry upon error."))
+  if (retry_times > 1) stop(add_ts("Only supports one level of retry upon error."))
   if (is.null(retry_counter)) { 
     retry_counter <- 0
   } else {
     retry_counter <- retry_counter + 1
   }
   
+  # Count files for progress reporter
+  n_fas <- length(unlist(flood_areas))
+  n_mths <- length(axn_mths)
+  n_lcs <- length(lc_files)
+  n_mdls <- length(shorebird_model_files_reallong)
+  
+  n_rst <- n_fas
+  n_imp <- n_fas * n_mths
+  n_wxl <- n_imp * n_lcs
+  n_fcl <- n_wxl * 2
+  n_prd <- n_imp * n_mdls
+  n_att <- n_fas
+  
   # Start progress reporter
-  if (is.null(p)) p <- progressor(steps = length(flood_areas), auto_finish = FALSE)
+  if (is.null(p)) {
+    p <- progressor(steps = n_rst + n_imp + n_wxl + n_fcl + n_prd + n_att, 
+                    auto_finish = FALSE)
+  }
   
   # Set reporting level
   if (verbose_level > 1) {
     prg_msg <- TRUE
     fxn_msg <- TRUE
-  } else if (verbose_level == 1) {
-    prg_msg <- TRUE
-    fxn_msg <- FALSE
-  } else {
+  } else if (verbose_level == 0) {
     prg_msg <- FALSE
     fxn_msg <- FALSE
-  }
+  } else {
+    prg_msg <- TRUE
+    fxn_msg <- FALSE
+  } 
   
   foreach(fa = flood_areas) %dofuture% {
     
@@ -66,29 +69,34 @@ evaluate_auction <- function(flood_areas, overwrite = FALSE,
     fxn <- "setup"
     lbl <- paste0(fa, " ")
     prg_mult <- 1
-    if (prg_msg) p(add_ts(lbl, "started"), class = "sticky", amount = 0)
+    if (prg_msg) p(add_ts(lbl, "started."), class = "sticky", amount = 0)
     
     # Streamlined error catching
-    tryCatch({
+    withCallingHandlers({
       
       # Get flood area files for specified fa
       fa_files <- list.files(spl_dir, pattern = ".shp$", full.names = TRUE)
       fa_files <- fa_files[grepl(paste0("((", paste0(fa, collapse = ")|("), "))"), fa_files)]
+      if (length(fa_files) == 0) stop("No matching flooding area shapefiles; check that split_flooding_area ran")
       
       # Rasterize and buffer flood areas
+      if (prg_msg) p(add_ts(lbl, "rasterizing..."), class = "sticky", amount = 0)
       fxn <- "rasterize"
       fa_rst_files <- rasterize_flooding_area(fa_files,
                                               guide_raster = ref_file,
                                               output_dir = spl_dir,         #defined in definitions.R
                                               buffer_dist = 10000,
                                               overwrite = overwrite_global,
-                                              verbose = TRUE)
+                                              verbose = fxn_msg)
       
       if (prg_msg) p(add_ts("Rasterized files (", length(fa_rst_files), "): ", 
                             paste0(basename(fa_rst_files), collapse = ", ")), 
                      class = "sticky", amount = 0)
+      p(amount = length(fa_rst_files) * global_prog_mult)
+      gc()
       
       # Impose flooding
+      if (prg_msg) p(add_ts(lbl, "imposing flooding..."), class = "sticky", amount = 0)
       fxn <- "impose-water"
       water_imp_files <- impose_flooding(lt_wtr_files,
                                          fa_rst_files,
@@ -100,7 +108,11 @@ evaluate_auction <- function(flood_areas, overwrite = FALSE,
       if (prg_msg) p(add_ts("Imposed files (", length(water_imp_files), "): ", 
                             paste0(basename(water_imp_files), collapse = ", ")), 
                      class = "sticky", amount = 0)
+      p(amount = length(water_imp_files) * global_prog_mult)
+      gc()
       
+      # Overlay water on landcover
+      if (prg_msg) p(add_ts(lbl, "overlaying water and landcover..."), class = "sticky", amount = 0)
       fxn <- "overlay-water-landcover"
       wxl_files <- overlay_water_landcover(water_imp_files, 
                                            lc_files,
@@ -111,26 +123,32 @@ evaluate_auction <- function(flood_areas, overwrite = FALSE,
       if (prg_msg) p(add_ts("WxL files (", length(wxl_files), "): ", 
                             paste0(basename(wxl_files), collapse = ", ")), 
                      class = "sticky", amount = 0)
-      #p(amount = 0.4 * prg_mult * global_prog_mult)
+      p(amount = length(wxl_files) * global_prog_mult)
+      gc()
       
+      # Calculate neighborhood water by landcover
+      if (prg_msg) p(add_ts(lbl, "calculating neighborhood water..."), class = "sticky", amount = 0)
       fxn <- "mean-neighborhood-water"
-      fcl_imp_files <- mean_neighborhood_water(wxl_files, #previously-created water x landcover files
+      imp_fcl_files <- mean_neighborhood_water(wxl_files, #previously-created water x landcover files
                                                distances = c(250, 5000), #250m and 5km
                                                output_dir = imp_fcl_dir,
                                                trim_extent = TRUE,  #only set for TRUE with splits
                                                overwrite = overwrite,
                                                verbose = fxn_msg)
       
-      if (prg_msg) p(add_ts("Focal files (", length(fcl_imp_files), "): ", 
-                            paste0(basename(fcl_imp_files), collapse = ", ")), 
+      if (prg_msg) p(add_ts("Focal files (", length(imp_fcl_files), "): ", 
+                            paste0(basename(imp_fcl_files), collapse = ", ")), 
                      class = "sticky", amount = 0)
-      #p(amount = 0.5 * prg_mult * global_prog_mult)
+      p(amount = length(imp_fcl_files) * global_prog_mult)
+      gc()
       
+      # Predict
+      if (prg_msg) p(add_ts(lbl, "predicting..."), class = "sticky", amount = 0)
       fxn <- "predict-bird-rasters"
-      prd_files <- predict_bird_rasters(water_files_realtime = fcl_imp_files,
-                                        water_files_longterm = fcl_avg_files,
-                                        scenarios = scenarios_filter,
-                                        water_months = mths,
+      prd_files <- predict_bird_rasters(water_files_realtime = imp_fcl_files,
+                                        water_files_longterm = lt_fcl_files,
+                                        scenarios = "imposed",
+                                        water_months = axn_mths,
                                         model_files = shorebird_model_files_reallong,
                                         model_names = shorebird_model_names_reallong,
                                         static_cov_files = bird_model_cov_files,
@@ -140,11 +158,15 @@ evaluate_auction <- function(flood_areas, overwrite = FALSE,
                                         monthly_cov_names = tmax_names,
                                         output_dir = imp_prd_dir,
                                         overwrite = overwrite,
-                                        verbose = fxn_msg) #1)
+                                        verbose = fxn_msg)
       
       if (prg_msg) p(add_ts("Pred files (", length(prd_files), "): ", paste0(basename(prd_files), collapse = ", ")), 
                      class = "sticky", amount = 0)
+      p(amount = length(prd_files) * global_prog_mult)
+      gc()
       
+      # Extract predictions
+      if (prg_msg) p(add_ts(lbl, "extracting predictions..."), class = "sticky", amount = 0)
       fxn <- "extract-predictions"
       stat_files <- extract_predictions(prd_files,
                                         fa_files,
@@ -156,8 +178,19 @@ evaluate_auction <- function(flood_areas, overwrite = FALSE,
       
       if (prg_msg) p(add_ts("Stat files (", length(stat_files), "): ", paste0(basename(stat_files), collapse = ", ")), 
                      class = "sticky", amount = 0)
+      p(amount = length(stat_files) * global_prog_mult)
+      gc()
       
+    # On interrupt
+    # Add explicitly to make interrupts more reliable
+    }, interrupt = function(i) {
+      
+      plan(sequential) #close orphan threads and release memory #may not work when called here
+      p(add_ts("USER INTERRUPT - Execution terminated."), class = "sticky", amount = 0)
+    
+    # On error 
     }, error = function(e) {
+      
       if (retry_counter == 0) {
         lbl <- ""
       } else if (retry_counter == 1) {
@@ -172,22 +205,13 @@ evaluate_auction <- function(flood_areas, overwrite = FALSE,
                                            "_retry-", retry_counter, "-of-", retry_times,
                                            "_date-", format(Sys.time(), format = "%Y-%m-%d"), ".rds")))
       
-      # If retry request, loop across individual flood areas
+      # If retry request, set overwrite to TRUE
       if (retry_times >= 1 & retry_counter == 0) {
         
-        p(add_ts("Retrying files in this set individually to find problematic file(s)..."),
-          class = "sticky", amount = 0)#.3 * prg_mult * -1)
-        evaluate_auction(unlist(fa), overwrite = overwrite,
-                         retry_times = min(retry_times, 2), retry_counter = retry_counter,
-                         p = p, global_prog_mult = 0, verbose_level = 1)
-        
-      # If repeating a second time, set overwrite to TRUE
-      } else if (retry_times > 1 & retry_counter == 1) {
-        
-        p(add_ts("Retrying problematic file with overwrite == TRUE..."), class = "sticky", amount = 0)#.3 * prg_mult * -1)
+        p(add_ts("Retrying problematic file with overwrite == TRUE..."), class = "sticky", amount = 0)
         evaluate_auction(unlist(fa), overwrite = TRUE,
-                         retry_times = min(retry_times, 2), retry_counter = retry_counter,
-                         p = p, global_prog_mult = 0, verbose_level = 2)
+                         retry_times = min(retry_times, 1), retry_counter = retry_counter,
+                         p = p, global_prog_mult = 0, verbose_level = 3)
         
       # If failed on last attempt (including an attempt that is first & only), remove progress for it
       } else {
@@ -196,6 +220,18 @@ evaluate_auction <- function(flood_areas, overwrite = FALSE,
         
       }
       
+    }, message = function(m) {
+      if (fxn_msg) {
+        msg <- m$message
+        if (verbose_level > 2) {
+          p(gsub("\\s", " ", msg), class = "sticky", amount = 0)
+        } else if (grepl("(Output file)|(Complete.)", msg)) {
+          #nothing
+          #idea for future: trigger progress based on text or class of raised condition
+        } else {
+          p(gsub("\\s", " ", msg), class = "sticky", amount = 0)
+        }
+      }
     }
     )
     
@@ -203,18 +239,30 @@ evaluate_auction <- function(flood_areas, overwrite = FALSE,
   
 }
 
-# Run sequentially (testing)
-plan(sequential)
-evaluation <- evaluate_auction(as.list(flood_areas[2]), verbose_level = 2, retry_times = 1)
+# Setup progress reporter
 
-# Run multisession (evaluation)
+handlers(global = TRUE)
+handlers(handler_progress(
+  format = "[:bar] :percent (:current/:total) - Elapsed: :elapsed, ETA: :eta", #:spin widget only spins when p() is called
+  clear = FALSE))
+
+# Run sequentially (for testing purposes)
+#plan(sequential)
+#evaluation <- evaluate_auction(flood_areas[1], verbose_level = 2, retry_times = 0, overwrite = FALSE)
+
+# Set number of cores to use if running 
+# flood_areas pulled from axn_shp in 01_setup.R
+cores_to_use <- 4
+n_sessions <- min(length(flood_areas), cores_to_use, cores_max_global, availableCores() - 1)
+
+# Run multisession evaluation
 plan(multisession, workers = n_sessions)
-evaluation <- evaluate_auction(as.list(flood_areas), verbose_level = 1, retry_times = 1, overwrite = FALSE)
+evaluation <- evaluate_auction(flood_areas, verbose_level = 1, retry_times = 0, overwrite = overwrite_global)
 
-# Sumamrize
+# Summarize
 sum_files <- summarize_predictions(stat_files, 
-                                   field_shapefile = axn_file_prj, 
+                                   field_shapefile = axn_file_clean, 
                                    output_dir = imp_stat_dir, 
-                                   overwrite = overwrite)
+                                   overwrite = TRUE)
 
 
